d48f97aad4eb4641855bc0740368f9f7e7244714
himaorg <work.himaorg@gmail.com>
2024-08-13 22:52:19 +0900

    ver.2.10.4

--- 04.todolist/js/todolist.js
+++ 04.todolist/js/todolist.js
@@ -44,31 +44,6 @@ document.addEventListener("DOMContentLoaded",function(){
     });
         
 
-
-
-
-
-
-    // // DOMが変化した時、それを、localStorageに保存する
-    // document.addEventListener("input",()=>{
-    //     localStorage.setItem("title",titleNode.value);
-    // });
-    // document.addEventListener("input",()=>{
-    //     localStorage.setItem("postAt",postAtNode.value);
-    // });
-    // ----------------------------------------------------------------------------------
-    // addEventListenerはforEachの外側に配置しろ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
-    // addEventListenerはforEachの外側に配置しろ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
-    // addEventListenerはforEachの外側に配置しろ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
-    // categoryNode.forEach((node,index)=>{
-        // document.addEventListener("input",()=>{
-            //     localStorage.setItem(`category${index}`,node.value);
-            // })});
-            // tagNode.forEach((node,index)=>{
-                // document.addEventListener("input",()=>{
-                    //     localStorage.setItem(`tag${index}`,node.value);
-                    // })});
-    // ----------------------------------------------------------------------------------
         // DOMが変化した時、それを、localStorageに保存する
         document.addEventListener("input", (event) => {
             // categoryNode に対しての処理
@@ -91,4 +66,90 @@ document.addEventListener("DOMContentLoaded",function(){
             localStorage.setItem("title", titleNode.value);
             localStorage.setItem("postAt", postAtNode.value);
         });
-    // ----------------------------------------------------------------------------------
+
+
+
+
+
+        // リストをクリックして削除
+        let selected = null;
+
+        // .mainにclickイベントを付与
+        document.querySelectorAll(".main").forEach(the_clicked_todolist=>{
+            the_clicked_todolist.addEventListener("click",function(){
+                // 既に選択されている（nullではない場合、）それを解除
+                if(selected){
+                    selected.classList.remove("selected");
+                }
+                // 今クリックした.mainに、（thisを使って）.selectedを付与する
+                selected = this;
+                selected.classList.add("selected");
+            });
+        });
+
+
+        // selectedが付与された.mainを削除するためのボタンのイベントを作る
+        document.getElementById("delete").addEventListener("click",function(){
+            if(selected){
+                // remove()を使って、selectedが付与された.mainを削除する
+                selected.remove();
+                // 変数を空にする。この変数に紐付けられたcssもリセットされる。
+                selected.null;
+            }
+        });
+
+
+        // // #addのbottonをクリックして、.mainを追加するイベント
+        // document.getElementById("add").addEventListener("click",function(){
+        //     // 取得
+        //     const template = document.getElementById("template");
+        //     // コピー
+        //     const clone = template.cloneNode("true");
+        //     // IDを削除
+        //     clone.id = "";
+        //     // DOMに追加
+        //     document.body.appendChild(clone);
+        // });
+
+        // insertAdjacentHTMLを使って、準備したHTMLをDOMに表示する為の処理
+            // クリックイベントでinsertを呼び出す
+            document.getElementById("add").addEventListener("click",function(){
+                // 準備したHTMLを格納する関数を作る
+                const insert = `
+                    <!-- ▼▼ひな型▼▼ -->
+                    <div class="main">
+                        <!-- ▼▼▼title▼▼▼ -->
+                        <div class="title-wrap">
+                            <textarea class="title-node"></textarea>
+                                
+                        </div>
+                        <!-- ▲▲▲title▲▲▲ -->
+                        <!-- ▼▼▼PostAt▼▼▼ -->
+                        <div class="post-at-wrap">
+                            <label for="post-at-node">投稿日　：</label>
+                            <input class="post-at-node">
+                        </div>
+                        <!-- ▲▲▲PostAt▲▲▲ -->
+                        <!-- ▼▼▼Category▼▼▼ -->
+                        <div class="category-wrap">
+                            <label for="category-node">カテゴリ：</label>
+                            <input class="category-node">
+                            <input class="category-node">
+                            <input class="category-node">
+                        </div>
+                        <!-- ▲▲▲Category▲▲▲ -->
+                        <!-- ▼▼▼tag▼▼▼ -->
+                        <div class="tag-wrap">
+                            <label for="tag-node">タグ　　：</label>
+                            <input class="tag-node">
+                            <input class="tag-node">
+                            <input class="tag-node">
+                        </div>
+                        <!-- ▲▲▲tag▲▲▲ -->
+                    </div>
+                    <!-- ▲▲▲ひな型▲▲▲ -->
+                `;
+            // insertAdjacentHTMLを使う
+            document.getElementById("ToDoList").insertAdjacentHTML("beforeend",insert);
+            }
+            );
