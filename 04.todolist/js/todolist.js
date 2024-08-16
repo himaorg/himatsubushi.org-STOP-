e48bc2fc2b229e7572e58418c924a5cfa84066e8
himaorg <work.himaorg@gmail.com>
2024-08-16 21:00:33 +0900

    ver.2.10.5

--- 04.todolist/js/todolist.js
+++ 04.todolist/js/todolist.js
@@ -99,17 +99,6 @@ document.addEventListener("DOMContentLoaded",function(){
         });
 
 
-        // // #addのbottonをクリックして、.mainを追加するイベント
-        // document.getElementById("add").addEventListener("click",function(){
-        //     // 取得
-        //     const template = document.getElementById("template");
-        //     // コピー
-        //     const clone = template.cloneNode("true");
-        //     // IDを削除
-        //     clone.id = "";
-        //     // DOMに追加
-        //     document.body.appendChild(clone);
-        // });
 
         // insertAdjacentHTMLを使って、準備したHTMLをDOMに表示する為の処理
             // クリックイベントでinsertを呼び出す
@@ -151,5 +140,135 @@ document.addEventListener("DOMContentLoaded",function(){
                 `;
             // insertAdjacentHTMLを使う
             document.getElementById("ToDoList").insertAdjacentHTML("beforeend",insert);
+
+
+            // inserAdjeccentHTMLで追加したHTMLにクリックイベントを付与する
+            const newList = document.querySelectorAll(".main:last-child");
+            // 実行する
+            attachClickEvent(newList);
             }
             );
+
+
+
+
+            // attachClickEventを宣言する。.selectedを付与、removeとaddに関わる処理を記述
+            // attachClickEventに、elementsという引数を渡す。elementsとは、ボタンで追加されたリストたちのことである
+            function attachClickEvent(elements){
+                // forEachにelementという引数を渡す。elementを人身御供にして順番に処理していく
+                elements.forEach(element => {
+                    element.addEventListener("click",function(){
+                        if(selected){
+                            selected.classList.remove("selected");
+                        }
+                        selected = this;
+                        selected.classList.add("selected");
+                    })
+                })
+            };
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+            // 二個目以降のリストを保存したり呼び出したりする処理（追加・削除を加えて）
+            // 実行する
+            localStorage(newList);
+            cool(newList);
+            category(newList);
+            tag(newList);
+            other(newList);
+
+            // 追加・削除を保存
+            function localStorage(elements){
+                elements.forEach(element => {
+                    element.addEventListener("",function(){
+                        
+                    })
+                })
+            }
+
+            // 呼び出す
+            function cool(elements){
+                elements.forEach(element =>{
+                    element.addEventListener("DOMContentLoaded",function(){
+
+
+                        // queryselectorで要素を取得
+                        titleNode = document.querySelector(".title-node");
+                        postAtNode = document.querySelector(".post-at-node");
+                        categoryNode = document.querySelectorAll(".category-node");
+                        tagNode = document.querySelectorAll(".tag-node");
+
+                        // デバッグ用
+                        if (!titleNode) {
+                            console.error("titleNode が見つかりませんでした。");
+                        }
+                        if (!postAtNode) {
+                            console.error("postAtNode が見つかりませんでした。");
+                        }
+                        console.log(categoryNode);
+                        console.log(tagNode);
+
+                        // もし、ローカルストレージにtitleというkeyが存在するなら、
+                        if(localStorage.getItem("title")){
+                            // title-nodeに、ローカルストレージに保存されている"titleNode"を表示する
+                            titleNode.value=localStorage.getItem("title");
+                        }
+
+                        if(localStorage.getItem("postAt")){
+                            postAtNode.value=localStorage.getItem("postAt");
+                        };
+
+                        categoryNode.forEach((node,index)=>{
+                        if(localStorage.getItem(`category${index}`)){
+                            node.value=localStorage.getItem(`category${index}`);
+                        }});
+
+                        tagNode.forEach((node,index)=>{
+                        if(localStorage.getItem(`tag${index}`)){
+                            node.value=localStorage.getItem(`tag${index}`);
+                        }});
+
+                        
+                    })
+                })
+            }
+
+
+
+            // 保存する
+            // category
+            function category(elements){
+                elements.forEach(element => {
+                    element.addEventListener("input",(event)=>{
+                        categoryNode.forEach((node,index)=>{
+                            if(event.target === node){
+                                localStorage.setItem(`category${index}`,node.value);
+                            }
+                        })
+                    })
+                })
+            }
+            // tag
+            function tag(elements){
+                elements.forEach(element => {
+                    element.addEventListener("input",(event)=>{
+                        categoryNode.forEach((node,index)=>{
+                            if(event.target === node){
+                                localStorage.setItem(`tag${index}`,node.value);
+                            }
+                        })
+                    })
+                })
+            }
+            // その他 other
+            function other(elements){
+                elements.forEach(element =>{
+                    element.addEventListener("input",()=>{
+                        localStorage.setItem("title", titleNode.value);
+                        localStorage.setItem("postAt", postAtNode.value);            
+                    })
+                })
+            }
+
